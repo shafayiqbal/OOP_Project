0406ac382906a54eb2384644aaef963d0f062265
UzairAmin101 <44573912+UzairAmin101@users.noreply.github.com>
2021-11-25 13:30:48 +0500

    Update Jamalo.cpp

--- Jamalo/Jamalo.cpp
+++ Jamalo/Jamalo.cpp
@@ -1,5 +1,8 @@
 #include "Jamalo.hpp"
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 Jamalo::Jamalo(SDL_Renderer *renderer, SDL_Texture *asst) : j(renderer, asst), gRenderer(renderer), assets(asst)
 {
@@ -20,6 +23,17 @@ void Jamalo::createObjects(int x, int y)
     SDL_Rect mov = {x, y, 100, 35};
 
     // Add random vehicle generator function
+
+    int prob = rand() % 3 + 1;
+    int car_prob;
+    if (prob == 2)
+    {
+        car_prob = rand() % 5 + 1;
+        switch (car_prob)
+        {
+        case 1:
+        }
+        }
 }
 
 void Jamalo::move(char x)
