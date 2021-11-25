3b28a8833b2de37b3b29faef0392e8beb073550a
UzairAmin101 <44573912+UzairAmin101@users.noreply.github.com>
2021-11-25 12:17:10 +0500

    some changes

--- Jamalo/Vehicle.cpp
+++ Jamalo/Vehicle.cpp
@@ -1,8 +1,9 @@
 #include "Vehicle.hpp"
 
-Vehicle::Vehicle(SDL_Renderer *rend, SDL_Texture *ast, SDL_Rect mov, SDL_Rect src) : Unit(rend, ast), src(src) {}
+Vehicle::Vehicle(SDL_Renderer *rend, SDL_Texture *ast, SDL_Rect mov, SDL_Rect src) : Unit(rend, ast), src(src), mover(mov) {}
 
 void Vehicle::draw()
 {
+    mover.x -= 10;
     Unit::draw(src, mover);
 }
